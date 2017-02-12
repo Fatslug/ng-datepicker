31c5f3e65885d4f7e2fcf50c0d8d383526750e35
Fatslug <sean@fatslug.ca>
2017-02-12 02:58:01 -0500

    Removing console.logs()

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// IMPORT DATEPICKER
+import { DatepickerComponent } from './datepicker/datepicker.component';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+	AppComponent,
+	DatepickerComponent
+],
+imports: [
+	BrowserModule,
+	FormsModule,
+	HttpModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
